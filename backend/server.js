ca5ff477c1249ca0c481784659b74371d033d309
isoo0301 <lauren030109@gmail.com>
2025-05-29 08:58:47 -0500

    setting backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,15 @@
+
 // Just note that the example backend is not using 'await' therefore not using multi threading
 
 // we should aim to use multi threading
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
---
+++ backend/test.js
@@ -0,0 +1,4 @@
+console.log("Hello, this is a test");
+setInterval(() => {
+  console.log("Still running...");
+}, 1000);
